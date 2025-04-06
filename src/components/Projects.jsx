b1355df0bec302ad336f4c52089da7e255b6e9b3
Anshul Kushwah <93611786+anshul-010@users.noreply.github.com>
2025-04-06 22:29:41 +0530

    Update Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,34 +2,28 @@ import React from "react";
 import { Card } from "./Card";
 import Trendzy from "../assets/img/projects/TrendzyProject.png";
 import Treders from "../assets/img/projects/TredersProject.png";
-import HealthMart from "../assets/img/projects/healthMartProject.png";
 import CreditFolio from "../assets/img/projects/creditFolio.png"
 import "./Project.css";
 export const Projects = () => {
   const project = {
     data: [
+      
       {
-        name: "Trendzy",
-        desc: "Clone of meesho.com which is a Fashion based E-commerce Platform",
-        img: Trendzy,
-        demoLink: "https://66cc670d37ef603b92569f29--magical-centaur-29595a.netlify.app/",
-        codeLink: "https://github.com/anshul-010/Trendzy-201",
+        name: "Man Matters",
+        desc: "Your one-stop destination for personalized health consultations with experienced doctors and curated products for skin, hair, and beard care.",
+        img: "https://github-production-user-asset-6210df.s3.amazonaws.com/93611786/430722396-d7c47606-00e4-4435-aa7c-1ba91f944e87.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20250406%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20250406T165749Z&X-Amz-Expires=300&X-Amz-Signature=252708253d47b6f2786f46ed8864fc46fa6047436f24360b9bd9ccdd3752f064&X-Amz-SignedHeaders=host",
+        demoLink: "https://man-matters-drab.vercel.app/",
+        codeLink: "https://github.com/anshul-010/Man-Matters",
         techstack: [
-          "HTML, ",
+          "React.js ",
+          "Redux ",
           "CSS, ",
-          "Javascript",
-        ],
-      },
-      {
-        name: "Health Mart",
-        desc: "Clone of HealthKart.com which is an E-commerce website delivering Healthy Products & Supplements",
-        img: HealthMart,
-        demoLink: "https://clinquant-otter-d2612d.netlify.app/",
-        codeLink: "https://github.com/Vikas6865/fond-channel-7524",
-        techstack: [
-          "HTML, ",
-          "CSS, ",
-          "Javascript",
+          "ChakraUI ",
+          "Javascript ",
+          "Node.js ",
+          "Express ",
+          "Mongo DB "
+
         ],
       },
       {
@@ -44,6 +38,18 @@ export const Projects = () => {
           "Javascript, ",
           "React, ",
           "Chakra UI"
+        ],
+      },
+      {
+        name: "Trendzy",
+        desc: "Clone of meesho.com which is a Fashion based E-commerce Platform",
+        img: Trendzy,
+        demoLink: "https://66cc670d37ef603b92569f29--magical-centaur-29595a.netlify.app/",
+        codeLink: "https://github.com/anshul-010/Trendzy-201",
+        techstack: [
+          "HTML, ",
+          "CSS, ",
+          "Javascript",
         ],
       },
       {
